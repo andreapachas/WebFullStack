5bc27cef886e27b758844c38fcdc1e5e0ae9f01e
Andrea <andrea.pachas@gmail.com>
2020-11-13 00:26:56 -0500

    add dark and light theme and render Gifos

--- Projects/Gifos/js/dist/main.dev.js
+++ Projects/Gifos/js/dist/main.dev.js
@@ -1,12 +1,44 @@
 "use strict";
 
+// change mode theme
 var API_KEY = 'Z1cPvuxq0X6YlUbbKcrSvNby4CgLf8XQ';
-var container = document.querySelector(".containerTrending");
+var containerTrending = document.querySelector(".containerTrending");
+var buttonChangeTheme = document.querySelector(".buttonChangeTheme");
+var navOptions = document.querySelector(".nav-options");
+var searchbar1 = navOptions.querySelector(".searchBar");
+var app = document.querySelector(".app");
+var themeName = document.querySelector(".theme-name");
+var logo = document.querySelector(".logo");
+var crearGifos = document.querySelector(".crearGifos");
+var iconSearch = document.querySelectorAll(".iconSearch");
+var iconClose = document.querySelectorAll(".iconClose");
+var buttonSliderLeft = document.querySelectorAll(".buttonSliderLeft");
+var buttonSliderRight = document.querySelectorAll(".buttonSliderRight"); // search
+
+var formGifo = document.getElementById('searchBar1');
+var formGifoNav = document.getElementById('searchBar2');
+var inputSearch = document.getElementById("inputSearch");
+var inputSearchNav = document.getElementById("inputSearchNav"); // search - results
+
+var offset = 0;
+var titleCategorySearch = document.getElementById("titleCategorySearch");
+var contentResultsGifos = document.querySelector(".contentResultsGifos"); //buttons
+
+var MoreGifs = document.getElementById("MoreGifs");
+var moreFavorites = document.getElementById("moreFavorites");
+var moreMyGifos = document.getElementById("moreMyGifos"); // sections
+
+var searchingPage = document.querySelector(".searching-page");
+var favorites = document.querySelector(".favorites");
+var myGifos = document.querySelector(".myGifos");
+var trendingGifos = document.querySelector(".trendingGifos"); // falta modal y crear Gifos
+
+var currentTheme = app.classList.contains("light") ? "light" : "dark"; //add trending Titles
 
 function renderTrendingTitle() {
-  function showResult() {
+  function showTrendigTopic() {
     var url, response, results;
-    return regeneratorRuntime.async(function showResult$(_context) {
+    return regeneratorRuntime.async(function showTrendigTopic$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
@@ -21,10 +53,9 @@ function renderTrendingTitle() {
 
           case 6:
             results = _context.sent;
-            console.log(results);
             if (results.meta.msg === 'OK') results.data.slice(0, 5).forEach(renderResult);else alert('not found');
 
-          case 9:
+          case 8:
           case "end":
             return _context.stop();
         }
@@ -32,14 +63,125 @@ function renderTrendingTitle() {
     });
   }
 
-  function renderResult(result, index) {
-    var plot = document.createElement('p');
-    plot.textContent = result;
-    if (index < 4) plot.textContent += ',';
-    container.appendChild(plot);
+  function showRenderTrendingTittle(result) {
+    removeAllChildNodes(contentResultsGifos);
+    showSearchResult(result);
   }
 
-  showResult();
+  function renderResult(result, index) {
+    var tittle = document.createElement('p');
+    tittle.textContent = result;
+    tittle.addEventListener("click", function () {
+      return showRenderTrendingTittle(result);
+    });
+    if (index < 4) tittle.textContent += ',';
+    containerTrending.appendChild(tittle);
+  }
+
+  showTrendigTopic();
 }
 
-renderTrendingTitle();
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+} //show search results
+
+
+formGifo.addEventListener('submit', function () {
+  return showSearchResult(inputSearch.value);
+});
+formGifoNav.addEventListener('submit', function () {
+  return showSearchResult(inputSearchNav.value);
+});
+
+function showSearchResult(value) {
+  var url, response, results;
+  return regeneratorRuntime.async(function showSearchResult$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          if (!(value !== "")) {
+            _context2.next = 12;
+            break;
+          }
+
+          url = "https://api.giphy.com/v1/gifs/search?api_key=".concat(API_KEY, "&q=").concat(value, "&limit=12&offset=").concat(offset);
+          _context2.next = 4;
+          return regeneratorRuntime.awrap(fetch(url));
+
+        case 4:
+          response = _context2.sent;
+          _context2.next = 7;
+          return regeneratorRuntime.awrap(response.json());
+
+        case 7:
+          results = _context2.sent;
+          titleCategorySearch.style.display = "block";
+          titleCategorySearch.textContent = value;
+          console.log(results);
+
+          if (results.meta.msg === 'OK') {
+            results.data.forEach(renderSearchResultContent);
+            MoreGifs.style.display = "block";
+          } else alert('not found');
+
+        case 12:
+        case "end":
+          return _context2.stop();
+      }
+    }
+  });
+}
+
+function renderSearchResultContent(result) {
+  renderSearchResult(result, contentResultsGifos);
+}
+
+function renderSearchResult(result, container) {
+  var li = document.createElement('li');
+  var figure = document.createElement('figure');
+  var img = document.createElement('img');
+  li.className = 'result';
+  img.src = result.images.downsized.url;
+  img.alt = result.title;
+  figure.appendChild(img);
+  li.appendChild(figure);
+  container.appendChild(li);
+}
+
+renderTrendingTitle(); //Display navbar search
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    searchbar1.style.display = "block";
+  } else {
+    searchbar1.style.display = "none";
+  }
+} //load change theme dark and light
+
+
+buttonChangeTheme.addEventListener("click", function () {
+  app.classList.remove(currentTheme);
+  currentTheme = currentTheme === "light" ? "dark" : "light";
+  app.classList.add(currentTheme);
+  themeName.innerText = currentTheme === "light" ? "Modo Diurno" : "Modo Nocturno";
+  logo.src = currentTheme === "light" ? "images/logo-desktop.svg" : "images/Logo-modo-noc.svg";
+  crearGifos.src = currentTheme === "light" ? "images/button-crear-gifo.svg" : "images/CTA-crar-gifo-modo-noc.svg";
+  iconSearch.forEach(function (item) {
+    item.src = currentTheme === "light" ? "images/icon-search.svg" : "images/icon-search-mod-noc.svg";
+  });
+  iconClose.forEach(function (item) {
+    item.src = currentTheme === "light" ? "images/close.svg" : "images/close-modo-noct.svg";
+  });
+  buttonSliderLeft.forEach(function (item) {
+    item.src = currentTheme === "light" ? "images/button-slider-left.svg" : "images/button-slider-left-md-noct.svg";
+  });
+  buttonSliderRight.forEach(function (item) {
+    item.src = currentTheme === "light" ? "images/Button-Slider-right.svg" : "images/Button-Slider-right-md-noct.svg";
+  });
+});
--- Projects/Gifos/js/main.js
+++ Projects/Gifos/js/main.js
@@ -1,26 +1,139 @@
-const API_KEY = 'Z1cPvuxq0X6YlUbbKcrSvNby4CgLf8XQ'
+// change mode theme
+const API_KEY = 'Z1cPvuxq0X6YlUbbKcrSvNby4CgLf8XQ';
+const containerTrending = document.querySelector(".containerTrending");
+const buttonChangeTheme = document.querySelector(".buttonChangeTheme");
+const navOptions = document.querySelector(".nav-options");
+const searchbar1 = navOptions.querySelector(".searchBar");
+const app = document.querySelector(".app");
+const themeName = document.querySelector(".theme-name");
+const logo = document.querySelector(".logo");
+const crearGifos = document.querySelector(".crearGifos");
+const iconSearch = document.querySelectorAll(".iconSearch");
+const iconClose = document.querySelectorAll(".iconClose");
+const buttonSliderLeft = document.querySelectorAll(".buttonSliderLeft");
+const buttonSliderRight = document.querySelectorAll(".buttonSliderRight");
 
-const container = document.querySelector(".containerTrending");
+// search
+const formGifo = document.getElementById('searchBar1');
+const formGifoNav = document.getElementById('searchBar2');
+const inputSearch = document.getElementById("inputSearch");
+const inputSearchNav = document.getElementById("inputSearchNav");
 
+// search - results
+let offset = 0;
+const titleCategorySearch = document.getElementById("titleCategorySearch");
+const contentResultsGifos = document.querySelector(".contentResultsGifos");
+
+//buttons
+const MoreGifs = document.getElementById("MoreGifs");
+const moreFavorites = document.getElementById("moreFavorites");
+const moreMyGifos = document.getElementById("moreMyGifos");
+
+
+// sections
+const searchingPage = document.querySelector(".searching-page");
+const favorites = document.querySelector(".favorites");
+const myGifos = document.querySelector(".myGifos");
+const trendingGifos = document.querySelector(".trendingGifos");
+// falta modal y crear Gifos
+
+
+
+let currentTheme = app.classList.contains("light") ? "light" : "dark";
+//add trending Titles
 function renderTrendingTitle(){
-    async function showResult() {
+    async function showTrendigTopic() {
         const url = `https://api.giphy.com/v1/trending/searches?api_key=${API_KEY}&limit=5&rating=g`;
         const response = await fetch(url);
         const results = await response.json();
-        console.log(results);
         if(results.meta.msg === 'OK')
             results.data.slice(0,5).forEach(renderResult);
         else alert('not found');
     }
-
-    function renderResult(result,index) {
-        const plot = document.createElement('p');
-        plot.textContent = result;
-        if (index < 4) 
-            plot.textContent +=  ',';
-        container.appendChild(plot);
+    function showRenderTrendingTittle(result) {
+        removeAllChildNodes(contentResultsGifos);
+        showSearchResult(result);
     }
-    showResult();
+    function renderResult(result,index) {
+        const tittle = document.createElement('p');
+        tittle.textContent = result;
+        tittle.addEventListener("click", () => showRenderTrendingTittle(result));
+        if (index < 4) 
+            tittle.textContent +=  ',';
+        containerTrending.appendChild(tittle);
+    }
+    showTrendigTopic();
 }
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+//show search results
+formGifo.addEventListener('submit', () => showSearchResult(inputSearch.value));
+formGifoNav.addEventListener('submit',  () => showSearchResult(inputSearchNav.value));
+async function showSearchResult (value)
+{
+    if (value !== "") {
+        const url =`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${value}&limit=12&offset=${offset}`
+        const response = await fetch(url);
+        const results = await response.json();
+        titleCategorySearch.style.display = "block";
+        titleCategorySearch.textContent = value;
+        console.log(results);
+        if(results.meta.msg === 'OK'){
+            results.data.forEach(renderSearchResultContent);
+            MoreGifs.style.display = "block";
+        }
+        else alert('not found');
+    }
+}
+function renderSearchResultContent(result) { 
+    renderSearchResult(result,contentResultsGifos);
+}
+function renderSearchResult (result, container) {
+    const li = document.createElement('li');
+    const figure = document.createElement('figure');
+    const img = document.createElement('img');
 
+    li.className = 'result';
+    img.src = result.images.downsized.url;
+    img.alt = result.title;
+    
+
+    figure.appendChild(img);
+    li.appendChild(figure);
+    container.appendChild(li);
+}
 renderTrendingTitle();
+//Display navbar search
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    searchbar1.style.display = "block";
+  } 
+  else {
+    searchbar1.style.display = "none";
+  }
+}
+//load change theme dark and light
+buttonChangeTheme.addEventListener("click", () =>  {
+    app.classList.remove(currentTheme);
+    currentTheme = currentTheme === "light" ? "dark" : "light";
+    app.classList.add(currentTheme);
+    themeName.innerText = currentTheme === "light"? "Modo Diurno":"Modo Nocturno";
+    logo.src = currentTheme === "light"? ("images/logo-desktop.svg"): ("images/Logo-modo-noc.svg");
+    crearGifos.src  = currentTheme === "light"? ("images/button-crear-gifo.svg"): ("images/CTA-crar-gifo-modo-noc.svg");
+    iconSearch.forEach(function(item) {
+        item.src  = currentTheme === "light"? ("images/icon-search.svg"): ("images/icon-search-mod-noc.svg");
+    })
+    iconClose.forEach(function(item) {
+        item.src  = currentTheme === "light"? ("images/close.svg"): ("images/close-modo-noct.svg");
+    })
+    buttonSliderLeft.forEach(function(item) {
+        item.src  = currentTheme === "light"? ("images/button-slider-left.svg"): ("images/button-slider-left-md-noct.svg");
+    })
+    buttonSliderRight.forEach(function(item) {
+        item.src  = currentTheme === "light"? ("images/Button-Slider-right.svg"): ("images/Button-Slider-right-md-noct.svg");
+    })
+});
